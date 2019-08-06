bd8a1389230eeda6456dc64cda66f8fb36da12b5
Ryland <rylanddegregory@me.com>
2019-08-06 11:53:22 -0400

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,1 +1,3 @@
-Set-PSReadlineOption -TokenKind command -ForegroundColor DarkYellow
+File location: /Users/100043547/.config/powershell/Microsoft.PowerShell_profile.ps1
+---
+Set-PSReadLineOption -Colors @{ "Command" = [ConsoleColor]::DarkYellow}
