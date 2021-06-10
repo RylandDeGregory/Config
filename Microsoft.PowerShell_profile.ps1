9f0c389c62e3399a99c5997f59684d67c56cdaff
RylandDeGregory <rylanddegregory@me.com>
2021-06-10 17:25:58 -0400

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,4 @@
-File location: /Users/<username>/.config/powershell/Microsoft.PowerShell_profile.ps1
----
+#File location: /Users/<username>/.config/powershell/Microsoft.PowerShell_profile.ps1
 Set-PoshPrompt -Theme "~/.oh-my-posh-theme.omp.json"
 Set-PSReadLineOption -Colors @{ "Command" = [ConsoleColor]::DarkYellow }
 Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
