ba7644219ef7ee124eefeca4440cf62f9b110f7f
RylandDeGregory <rylanddegregory@me.com>
2022-01-25 18:42:09 -0500

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,5 @@
 #File location: /Users/<username>/.config/powershell/Microsoft.PowerShell_profile.ps1
+#requires -Version 7
 Set-PoshPrompt -Theme '~/.oh-my-posh-theme.omp.json'
 
 Set-PSReadLineOption -Colors @{ 'Command' = [ConsoleColor]::DarkYellow }
